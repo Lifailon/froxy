a8c51dbcdad4599325a676bedb40693e8addb952
Lifailon <deadepicboss@gmail.com>
2024-03-06 15:52:32 +0300

    added loging and exec

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Reverse Proxy .NET (rpnet)
 
-> 💡 At the functional testing stage
-
 [➡️](https://github.com/Lifailon/ReverseProxyNET/blob/rsa/README.md) Choose language [➡️‍](https://github.com/Lifailon/ReverseProxyNET/blob/rsa/README_RU.md) Выберите язык
 
 A command line utility for implementing a .NET based reverse proxy. It is used to provide access to all hosts on the network from one network interface to remote applications via HTTP and TCP protocols accessible through another network interface without unnecessary settings and with authorization support.
@@ -10,21 +8,146 @@ A command line utility for implementing a .NET based reverse proxy. It is used t
 
 This utility solves several problems:
 
-- Firstly, if you use a VPN server to access a specific URL resource (HTTP/HTTPS) on your machine, and want to provide access to it to other machines without using Proxy servers, which require additional configuration on the client side.
+- Firstly, if you are using a VPN server in point-to-point mode, and want to give the remote machine access to applications on other machines on your network running via the TCP protocol, for example, for the RDP, SSH or RTSP protocols. This also acts as an alternative to classic ssh tunneling, for example, via `OpenSSH` or `Putty`.
 
-- Secondly, if you are using a VPN server in point-to-point mode, and want to give the remote machine access to applications on other machines on your network running via the TCP protocol, for example, for the RDP, SSH or RTSP protocols. This also acts as an alternative to classic ssh tunneling, for example, via `OpenSSH` or `Putty`.
+- Secondly, if you use a VPN server to access a specific URL resource (HTTP/HTTPS) on your machine, and want to provide access to it to other machines on the network without using Proxy servers, which require additional configuration on the client side.
 
-- 🔓 Thirdly, if your Web application or REST API server does not support authorization, then you can add [Base64](https://en.wikipedia.org/wiki/Base64) encryption for all clients, which obliges you to transmit authorization data in the request header. If you are using a browser, you will be provided with a corresponding login form 🔑.
+- 🔓 Thirdly, if your Web application or REST API server does not support authorization, then you can add [Base64](https://en.wikipedia.org/wiki/Base64) encryption for all clients, which requires passing in the header request authorization data. If you are using a browser, a corresponding form will be provided to complete basic authorization 🔑.
 
 There are many alternatives that provide similar functionality individually. For example, `ncat` on Windows (from [nmap](https://github.com/nmap/nmap)) and `socat` on Linux for TCP or [ReverseProxy](https://github.com/ilanyu/ReverseProxy) in Golang to redirect HTTP/HTTPS traffic.
 
-<!-- ## 🚀 Installation
+## 🚀 Installation
 
-## 📌 Using
+Download the portable version executable from the GitHub repository.
+
+## 📌 Usage
+
+Get Help:
 
 ```PowerShell
+.\rpnet.exe --help
 
+Reverse Proxy server base on .NET.
+
+Parameters:
+  -h, --help                       Show help.
+  -l, --local <address:port>       Address and port of the interface through which requests will be proxy.
+  -r, --remote <address:port/url>  HTTP/HTTPS address of the remote resource to which requests will be proxy.
+  -u, --userName <admin>           User name for authorization.
+  -p, --password <admin>           User password for authorization.
+
+Examples:
+  .\rpnet.exe -l 127.0.0.1:8443 -r 192.168.3.106:80
+  .\rpnet.exe -l 127.0.0.1:8443 -r https://kinozal.tv
+  .\rpnet.exe -l *:8443 -r https://kinozal.tv
+  .\rpnet.exe -local *:8443 -remote https://kinozal.tv -userName proxy -password admin
 ```
 
-## 📑 Log
--->
+### TCP
+
+Accepts requests on the interface with IP address `192.168.3.100` and port `8443` to redirect to a remote host with IP address `192.168.3.106`, where the application is running on port `80`.
+
+```csharp
+.\rpnet.exe --local 192.168.3.100:8444 --remote 192.168.3.106:80
+TCP protocol is used
+Listening on 192.168.3.100:8444 for forwarding to 192.168.3.106:80
+[13:40:23] 192.168.3.99:29829: [::ffff:192.168.3.106]:80
+[13:40:23] 192.168.3.99:29830: [::ffff:192.168.3.106]:80
+[13:40:23] 192.168.3.99:29833: [::ffff:192.168.3.106]:80
+[13:40:23] 192.168.3.99:29838: [::ffff:192.168.3.106]:80
+[13:40:23] 192.168.3.99:29840: [::ffff:192.168.3.106]:80
+[13:40:23] 192.168.3.99:29842: [::ffff:192.168.3.106]:80
+[13:40:24] 192.168.3.99:29846: [::ffff:192.168.3.106]:80
+[13:40:24] 192.168.3.99:29848: [::ffff:192.168.3.106]:80
+```
+
+> 💡 To listen to all network interfaces, use the * symbol instead of the local IP address (you need to run the console with administrator rights).
+
+### HTTP
+
+Accepts requests on the interface with IP address `192.168.3.100` and port `8443` to redirect to the remote url resource [Kinozal](https://kinozal.tv).
+
+In the example, the connection is made from a client with the IP address `192.168.3.99` using the `GET` method. Lists all the endpoints that the client contacts to load the home page.
+
+```csharp
+.\rpnet.exe --local 192.168.3.100:8443 --remote https://kinozal.tv
+HTTP protocol is used
+Listening on 192.168.3.100:8443 for forwarding to https://kinozal.tv
+Not authorization is used
+[12:06:15] 192.168.3.99 GET: /
+[12:06:16] 192.168.3.99 GET: /pic/0_kinozal.tv.css?v=3.4
+[12:06:16] 192.168.3.99 GET: /pic/jquery-3.6.3.min.js?v=1.1
+[12:06:16] 192.168.3.99 GET: /pic/use.js?v=3.7
+[12:06:16] 192.168.3.99 GET: /pic/logo3.gif
+[12:06:16] 192.168.3.99 GET: /pic/emty.gif
+[12:06:17] 192.168.3.99 GET: /pic/radio_ban.jpg
+[12:06:17] 192.168.3.99 GET: /pic/knz_love.gif
+[12:06:17] 192.168.3.99 GET: /pic/sbg.gif
+[12:06:17] 192.168.3.99 GET: /pic/l_portiere.gif
+[12:06:17] 192.168.3.99 GET: /pic/r_portiere.gif
+[12:06:17] 192.168.3.99 GET: /pic/dw2.png       
+[12:06:17] 192.168.3.99 GET: /pic/cat/11.gif
+[12:06:17] 192.168.3.99 GET: /pic/cat/8.gif 
+[12:06:18] 192.168.3.99 GET: /pic/cat/15.gif
+[12:06:18] 192.168.3.99 GET: /pic/cat/17.gif
+[12:06:18] 192.168.3.99 GET: /pic/status_icons.png
+[12:06:19] 192.168.3.99 GET: /pic/srch_l.png
+[12:06:19] 192.168.3.99 GET: /pic/srch_r2.png        
+[12:06:19] 192.168.3.99 GET: /pic/flags_all.png?v=1  
+[12:06:19] 192.168.3.99 GET: /i/poster/8/6/433686.jpg
+[12:06:19] 192.168.3.99 GET: /i/poster/1/9/1357519.jpg
+[12:06:19] 192.168.3.99 GET: /i/poster/2/1/975821.jpg
+[12:06:19] 192.168.3.99 GET: /pic/cat/24.gif
+[12:06:19] 192.168.3.99 GET: /i/poster/2/0/175220.jpg 
+[12:06:19] 192.168.3.99 GET: /i/poster/2/0/1255220.jpg
+[12:06:20] 192.168.3.99 GET: /pic/favicon.ico
+```
+
+### Authorization
+
+To use client-side authorization, you must fill in the appropriate parameters when starting the server. If the client transmits incorrect authorization data, this will be displayed in the log.
+
+```csharp
+.\rpnet.exe --local 192.168.3.100:8443 --remote https://kinozal.tv --userName proxy --password admin
+HTTP protocol is used
+Listening on 192.168.3.100:8443 for forwarding to https://kinozal.tv
+Authorization is used
+[15:43:39] 192.168.3.100 GET: /
+[15:43:39] 192.168.3.100 GET: Authorization error
+[15:43:40] 192.168.3.100 GET: /
+[15:43:40] 192.168.3.100 GET: Authorization form sent
+[15:43:42] 192.168.3.100 GET: /
+[15:43:42] 192.168.3.100 GET: Authorization error
+[15:43:43] 192.168.3.100 GET: /
+[15:43:43] 192.168.3.100 GET: Authorization form sent
+[15:43:47] 192.168.3.100 GET: /
+[15:43:48] 192.168.3.100 GET: /pic/0_kinozal.tv.css?v=3.4
+[15:43:48] 192.168.3.100 GET: /pic/jquery-3.6.3.min.js?v=1.1
+[15:43:48] 192.168.3.100 GET: /pic/use.js?v=3.7
+[15:43:48] 192.168.3.100 GET: /pic/logo3.gif
+[15:43:48] 192.168.3.100 GET: /pic/emty.gif
+[15:43:49] 192.168.3.100 GET: /pic/radio_ban.jpg
+[15:43:49] 192.168.3.100 GET: /pic/knz_love.gif
+[15:43:49] 192.168.3.100 GET: /pic/sbg.gif
+[15:43:49] 192.168.3.100 GET: /pic/l_portiere.gif
+[15:43:49] 192.168.3.100 GET: /pic/r_portiere.gif
+[15:43:49] 192.168.3.100 GET: /pic/dw2.png
+[15:43:49] 192.168.3.100 GET: /pic/cat/8.gif
+[15:43:50] 192.168.3.100 GET: /pic/cat/49.gif
+[15:43:50] 192.168.3.100 GET: /pic/cat/6.gif
+[15:43:50] 192.168.3.100 GET: /pic/cat/45.gif
+[15:43:50] 192.168.3.100 GET: /pic/cat/13.gif
+[15:43:50] 192.168.3.100 GET: /pic/status_icons.png
+[15:43:50] 192.168.3.100 GET: /pic/srch_l.png
+[15:43:50] 192.168.3.100 GET: /pic/srch_r2.png      
+[15:43:50] 192.168.3.100 GET: /pic/flags_all.png?v=1
+[15:43:51] 192.168.3.100 GET: /i/poster/0/8/1767008.jpg
+[15:43:51] 192.168.3.100 GET: /i/poster/1/7/1995717.jpg
+[15:43:51] 192.168.3.100 GET: /pic/cat/15.gif
+[15:43:51] 192.168.3.100 GET: /pic/cat/38.gif
+[15:43:51] 192.168.3.100 GET: /i/poster/2/0/1255220.jpg
+[15:43:51] 192.168.3.100 GET: /i/poster/2/6/2024026.jpg
+[15:43:51] 192.168.3.100 GET: /pic/cat/46.gif
+[15:43:52] 192.168.3.100 GET: /pic/cat/24.gif
+[15:43:52] 192.168.3.100 GET: /pic/favicon.ico
+```
---
+++ source/rpnet.cs
@@ -0,0 +1,228 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Text;
+
+class rpNet {
+    static async Task Main(string[] args) {
+        // Объявляем переменные для параметров
+        string userName = null;
+        string password = null;
+        string local_addr = null;
+        string local = null;
+        string remote = null;
+        // Анализируем аргументы командной строки
+        for (int i = 0; i < args.Length; i += 2) {
+            // Проверяем содержимое аргументов на соответствие
+            if (args[i].Contains("-h")) {
+                Console.WriteLine("Reverse Proxy server base on .NET.\n");
+                Console.WriteLine("Parameters:");
+                Console.WriteLine("  -h, --help                       Show help.");
+                Console.WriteLine("  -l, --local <address:port>       Address and port of the interface through which requests will be proxy.");
+                Console.WriteLine("  -r, --remote <address:port/url>  HTTP/HTTPS address of the remote resource to which requests will be proxy.");
+                Console.WriteLine("  -u, --userName <admin>           User name for authorization.");
+                Console.WriteLine("  -p, --password <admin>           User password for authorization.\n");
+                Console.WriteLine("Examples:");
+                Console.WriteLine(@"  .\rpnet.exe -l 127.0.0.1:8443 -r 192.168.3.106:80");
+                Console.WriteLine(@"  .\rpnet.exe -l 127.0.0.1:8443 -r https://kinozal.tv");
+                Console.WriteLine(@"  .\rpnet.exe -l *:8443 -r https://kinozal.tv");
+                Console.WriteLine(@"  .\rpnet.exe -local *:8443 -remote https://kinozal.tv -userName proxy -password admin");
+                Console.WriteLine();
+                return;
+            }
+            // Следующий номер индекса (1/3) меньше общего количества аргументов (4) и этот аргумент не содержит ключ
+            if ((args[i] == "-l" || args[i] == "--local") && i + 1 < args.Length && !args[i + 1].StartsWith("-")) {
+                local_addr = args[i + 1];
+                local = $"http://{local_addr}/";
+            }
+            else if ((args[i] == "-r" || args[i] == "--remote") && i + 1 < args.Length && !args[i + 1].StartsWith("-")) {
+                remote = args[i + 1];
+            }
+            else if ((args[i] == "-u" || args[i] == "--userName") && i + 1 < args.Length && !args[i + 1].StartsWith("-")) {
+                userName = args[i + 1];
+            }
+            else if ((args[i] == "-p" || args[i] == "--password") && i + 1 < args.Length && !args[i + 1].StartsWith("-")) {
+                password = args[i + 1];
+            }
+        }
+        // Проверяем, что были переданы все нужные аргументы для запуска
+        if (local == null || remote == null) {
+            Console.WriteLine("Usage: rpnet --local <address:port> --remote <address:port/url>");
+            return;
+        }
+        // Проверяем протокол для доступа к удаленному ресурсу
+        // TCP
+        if (!remote.StartsWith("http://") && !remote.StartsWith("https://")) {
+            try {
+                Console.WriteLine("TCP protocol is used");
+                // Забираем порт
+                string[] addr_split = local_addr.Split(':');
+                string local_string_addr = addr_split[0];
+                string string_port = addr_split[1];
+                int local_port = int.Parse(string_port);
+                string[] addr_split_remote = remote.Split(':');
+                string remote_addr = addr_split_remote[0];
+                string string_port_remote = addr_split_remote[1];
+                int remote_port = int.Parse(string_port_remote);
+                // Проверяем переданный адрес для прослушивания и создаем экземпляр TCP сокета
+                TcpListener listener;
+                if (local_string_addr == "*") {
+                    listener = new TcpListener(IPAddress.Any, local_port);
+                }
+                else {
+                    IPAddress local_ip_addr = IPAddress.Parse(local_string_addr);
+                    listener = new TcpListener(local_ip_addr, local_port);
+                }
+                listener.Start();
+                Console.WriteLine($"Listening on {local_string_addr}:{local_port} for forwarding to {remote}");
+                while (true) {
+                    // Принимаем входящее TCP подключение
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    // Создаем новый TCP клиент для подключения к удаленному хосту
+                    TcpClient remoteClient = new TcpClient();
+                    // Подключаемся к удаленному хосту асинхронно
+                    await remoteClient.ConnectAsync(remote_addr, remote_port);
+                    // Логируем запрос
+                    Console.WriteLine(
+                        $"[{DateTime.Now.ToString("HH:mm:ss")}] {client.Client.RemoteEndPoint}: {remoteClient.Client.RemoteEndPoint}"
+                    );
+                    // Получаем потоки данных для клиента и удаленного хоста
+                    var clientStream = client.GetStream();
+                    var remoteStream = remoteClient.GetStream();
+                    // Запускаем асинхронные задачи копирования данных между клиентом и удаленным хостом
+                    Task clientReadTask = clientStream.CopyToAsync(remoteStream);
+                    Task remoteReadTask = remoteStream.CopyToAsync(clientStream);
+                    // Ожидаем завершения всех асинхронных задач
+                    await Task.WhenAll(clientReadTask, remoteReadTask);
+                    // Закрываем подключение с клиентом и удаленным хостом
+                    client.Close();
+                    remoteClient.Close();
+                }
+            }
+            // Возврощать ошибку в случае проблемы с запуском, например, неправильно передан локальный адрес (требуемый адрес для своего контекста неверен)
+            catch (Exception ex) {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        // HTTP
+        else {
+            try {
+                Console.WriteLine("HTTP protocol is used");
+                // Создаем экземпляр HTTP сокета для прослушивания подключений
+                var server = new HttpListener();
+                // Передаем параметр локального url (на всех адресах: *) и запускаем
+                server.Prefixes.Add(local);
+                server.Start();
+                Console.WriteLine($"Listening on {local_addr} for forwarding to {remote}");
+                if (userName != null && password != null) {
+                    Console.WriteLine("Authorization is used");
+                }
+                else {
+                    Console.WriteLine("Not authorization is used");
+                }
+                // Бесконечный цикл для обработки запросов
+                while (true) {
+                    // Читаем контекст запроса асинхронно. Оператор await ожидает завершения асинхронной операции без блокировки основного потока выполнения
+                    var context = await server.GetContextAsync();
+                    // Метод HandleRequest выполняется параллельно, что позволяет серверу обрабатывать несколько запросов одновременно
+                    _ = HandleRequest(context, remote, userName, password);
+                }
+            }
+            // Возврощать ошибку в случае проблемы с запуском, например, нет доступа
+            catch (Exception ex) {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+
+    static async Task HandleRequest(HttpListenerContext context, string remote, string userName, string password) {
+        var request = context.Request;
+        var response = context.Response;
+        // Выводим информацию запросов в консоль (лог)
+        Console.WriteLine(
+            $"[{DateTime.Now.ToString("HH:mm:ss")}] {request.RemoteEndPoint.Address} " + // ({request.UserAgent})
+            $"{request.HttpMethod}: {request.RawUrl}"
+        );
+        // Используем проверку аутентификации, если переданы соответствующие параметры
+        if (userName != null && password != null) {
+            // Проверяем, содержит ли заголовок запроса информацию о базовой аутентификации
+            if (request.Headers["Authorization"] != null) {
+                // Получаем строку аутентификации из заголовка
+                string authHeader = request.Headers["Authorization"];
+                // Извлекаем базовую часть и декодируем ее
+                string encodedUsernamePassword = authHeader.Split(' ')[1];
+                byte[] decodedBytes = Convert.FromBase64String(encodedUsernamePassword);
+                string usernamePassword = Encoding.UTF8.GetString(decodedBytes);
+                // Разделяем имя пользователя и пароль
+                string[] parts = usernamePassword.Split(':');
+                string userName_remote = parts[0];
+                string password_remote = parts[1];
+                // Проверка данных
+                if (userName_remote == userName && password_remote == password) {
+                    // Продолжаем обработку запроса
+                }
+                else {
+                    // Возвращаем клиенту статус ошибки авторизации и логируем
+                    Console.WriteLine(
+                        $"[{DateTime.Now.ToString("HH:mm:ss")}] {request.RemoteEndPoint.Address} " + // ({request.UserAgent})
+                        $"{request.HttpMethod}: Authorization error"
+                    );
+                    response.StatusCode = 401;
+                    response.StatusDescription = "Unauthorized";
+                    response.OutputStream.Close();
+                    return;
+                }
+            }
+            else {
+                // Если заголовок Authorization отсутствует, отправляем клиенту запрос авторизации
+                Console.WriteLine(
+                    $"[{DateTime.Now.ToString("HH:mm:ss")}] {request.RemoteEndPoint.Address} " + // ({request.UserAgent})
+                    $"{request.HttpMethod}: Authorization form sent"
+                );
+                response.StatusCode = 401;
+                response.Headers.Add("WWW-Authenticate", "Basic realm=\"Secure Area\"");
+                response.OutputStream.Close();
+                return;
+            }
+        }
+        // Создаем WebClient для выполнения запроса к удаленному серверу
+        #pragma warning disable SYSLIB0014 // Тип или член устарел
+        using (var client = new WebClient()) {
+            // Добавляем заголовок User-Agent из запроса клиента
+            client.Headers.Add("User-Agent", request.UserAgent);
+            try {
+                // Выполняем запрос к удаленному серверу асинхронно
+                var requestData = await client.DownloadDataTaskAsync(remote + request.RawUrl);
+                // Отправляем полученные данные обратно клиенту
+                response.OutputStream.Write(requestData, 0, requestData.Length);
+                // Устанавливаем статус успешного выполнения
+                response.StatusCode = 200;
+            }
+            catch (WebException ex) {
+                if (ex.Response != null) {
+                    // Если возникла ошибка, отправляем соответствующий статус и сообщение об ошибке обратно клиенту
+                    var errorResponse = (HttpWebResponse)ex.Response;
+                    response.StatusCode = (int)errorResponse.StatusCode;
+                    using (var errorStream = errorResponse.GetResponseStream()) {
+                        errorStream.CopyTo(response.OutputStream);
+                    }
+                }
+                else {
+                    // Если возникла другая ошибка, отправляем статус 500 и сообщение об ошибке
+                    response.StatusCode = 500;
+                    var errorMessage = $"Error occurred: {ex.Message}";
+                    var errorData = System.Text.Encoding.UTF8.GetBytes(errorMessage);
+                    response.OutputStream.Write(errorData, 0, errorData.Length);
+                }
+            }
+        }
+        #pragma warning restore SYSLIB0014 // Тип или член устарел
+        // Логируем код ответа, только если есть ошибка
+        if (response.StatusCode != 200) {
+            Console.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] Error: {response.StatusCode}");
+        }
+        // Закрываем поток ответа
+        response.Close();
+    }
+}
